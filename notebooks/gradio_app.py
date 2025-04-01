19f52e710b06069279397ea7c0401f7e6c456681
Daniel Iturra <daniel.eduardo1610@gmail.com>
2025-04-01 19:58:06 -0300

    Testeando gradio con modelo deep learning

---
+++ notebooks/gradio_app.py
@@ -0,0 +1,40 @@
+import gradio as gr
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
+# Cargar modelo
+model = load_model("notebooks/checkpoints/mobilenetv2_cifar100_best.h5")
+
+# Etiquetas CIFAR-100 (ejemplo)
+labels = [
+    'apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle',
+    'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle',
+    'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur',
+    'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'computer_keyboard',
+    'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain',
+    'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree',
+    'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket',
+    'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider',
+    'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor',
+    'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm'
+]
+
+def predict(img):
+    img = img.resize((96, 96))
+    img_array = np.array(img).astype("float32")
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+    preds = model.predict(img_array)[0]
+    top_5 = preds.argsort()[-5:][::-1]
+    return {labels[i]: float(preds[i]) for i in top_5}
+
+gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="pil"),
+    outputs=gr.Label(num_top_classes=5),
+    title="Clasificador CIFAR-100",
+    description="Sube una imagen para predecir su clase"
+).launch(server_name="0.0.0.0", server_port=7860)
